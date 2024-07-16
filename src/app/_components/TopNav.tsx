c021fd803edabce63dbad07ef1b3863cf158d9c5
Chris <christopher.sarm15@gmail.com>
2024-07-16 18:14:28 -0400

    adding auth with clerk

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,10 +1,17 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs"
+import { Sign } from "crypto";
 
 export function TopNav() {
   return (
     <nav className="flex items-center justify-between border-b px-4 py-3 md:px-6">
         <div className="flex items-center gap-2">Deep Work</div>
         <div>
-            <div>Login</div>
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
         </div>
     </nav>
   );
